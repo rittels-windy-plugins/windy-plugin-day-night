6fc83432ab4fc73cc0ebe02a1e8d84bdabf98935
chris <root@ChrisDell.localdomain>
2025-06-27 12:04:41 +0200

    version

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-day-night',
-    version: '0.0.1',
+    version: '0.0.2',
     icon: '🌅',
     title: 'Day Night',
     description:
