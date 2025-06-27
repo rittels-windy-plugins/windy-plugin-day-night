9a901288cd5fb06665600905824611a23825257b
chris <root@ChrisDell.localdomain>
2025-06-27 16:14:52 +0200

    load bug

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-daynight',
-    version: '0.0.4',
+    version: '0.0.5',
     icon: '🌅',
     title: 'Day Night',
     description:
