08941b9fcdf77191fe30b7134756169ddedc6371
chris <root@ChrisDell.localdomain>
2025-06-27 15:57:47 +0200

    error loading svelte

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-day-night',
-    version: '0.0.2',
+    version: '0.0.3',
     icon: '🌅',
     title: 'Day Night',
     description:
