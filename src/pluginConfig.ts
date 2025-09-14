6c826b8fc4205a5ea84d41d9da24687e76fcca64
chris <root@ChrisHP.localdomain>
2025-09-14 22:34:35 +0200

    fixed tz bug

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-day-night',
-    version: '0.0.11',
+    version: '0.0.12',
     icon: '🌅',
     title: 'Day Night',
     description:
